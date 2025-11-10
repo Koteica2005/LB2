567fb062c593921751979fa6687b7e28ab85b054
Koteica2005 <kirilkot2000@gmail.com>
2025-11-10 23:36:26 +0200

    Ініціалізація тесту (гра обнуляється після перезапуску)

--- Quiz-main/QuizTests/QuizGameTests.cs
+++ Quiz-main/QuizTests/QuizGameTests.cs
@@ -2,3 +2,20 @@
 using Quiz; 
 
 namespace Quiz.Tests
+{
+        [Fact]
+        public void Restart_WhenCalled_ShouldResetAllStats()
+        {
+            var game = new QuizGame();
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(false, 15);
+            game.GiveAnswer(true, 5);
+
+            game.Restart();
+
+            Assert.Equal(0, game.Score);
+            Assert.Equal(0, game.CorrectAnswers);
+            Assert.Equal(0, game.TotalQuestionsAnswered);
+            Assert.Equal(0.0, game.CorrectAnswerPercentage);
+        }
+}
