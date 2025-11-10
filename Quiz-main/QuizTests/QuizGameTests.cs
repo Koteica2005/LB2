1f8d041af627d3710d4d5b69bedd6359014491bf
Koteica2005 <kirilkot2000@gmail.com>
2025-11-10 23:43:41 +0200

    Ініціалізація тесту (перевірка, що при одній правильній і одній неправильній відповіді повертає 50%)

--- Quiz-main/QuizTests/QuizGameTests.cs
+++ Quiz-main/QuizTests/QuizGameTests.cs
@@ -38,4 +38,14 @@ namespace Quiz.Tests
 
             Assert.Equal(75.0, game.CorrectAnswerPercentage, 2);
         }
+        [Fact]
+        public void CorrectAnswerPercentage_When1Of2_ShouldBe50()
+        {
+            var game = new QuizGame();
+
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(false, 10);
+
+            Assert.Equal(50.0, game.CorrectAnswerPercentage, 2);
+          }
 }
